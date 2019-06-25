99c783505de07cd15a9b704175a263fdbd0c939b
Neiva07 <lucasneiva07@gmail.com>
2019-06-25 07:25:05 -0300

    alert sent to the user when the ticket is validated

--- mobile/src/pages/RedeemTicket/index.js
+++ mobile/src/pages/RedeemTicket/index.js
@@ -1,6 +1,6 @@
 import React, { useContext, useCallback } from "react";
 import QRcode from "react-native-qrcode";
-import { View, TouchableOpacity, Text } from "react-native";
+import { View, TouchableOpacity, Text, Alert } from "react-native";
 import { TicketContext } from "../../context/Tickets";
 import styles from "./styles";
 import io from "socket.io-client";
@@ -25,14 +25,34 @@ const index = props => {
 
   const handleBack = useCallback(() => {
     socket.disconnect(true);
-    props.navigation.navigate("App");
+    Alert.alert(
+      "Ticket lido com sucesso!",
+      "Bom almoço e volte sempre :)",
+      [
+        {
+          text: "OK",
+          onPress: () => {
+            props.navigation.navigate("App");
+          }
+        }
+      ],
+      { cancelable: false }
+    );
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text 
-      style={{fontSize: 20, textAlign:"center", fontWeight: "bold", marginBottom: 20, color:"#000"}}>
-        Aproxime do leitor o código abaixo para resgatar seu ticket!</Text>
+      <Text
+        style={{
+          fontSize: 20,
+          textAlign: "center",
+          fontWeight: "bold",
+          marginBottom: 20,
+          color: "#000"
+        }}
+      >
+        Aproxime do leitor o código abaixo para resgatar seu ticket!
+      </Text>
       {tickets.length > 0 ? (
         <QRcode
           value={tickets[0].qr_code}
@@ -41,9 +61,9 @@ const index = props => {
           fgColor="white"
         />
       ) : null}
-        <TouchableOpacity style={styles.outButton} onPress={handleBack}>
-          <Text style={styles.outText}>Cancelar</Text>
-        </TouchableOpacity>
+      <TouchableOpacity style={styles.outButton} onPress={handleBack}>
+        <Text style={styles.outText}>Cancelar</Text>
+      </TouchableOpacity>
     </View>
   );
 };
